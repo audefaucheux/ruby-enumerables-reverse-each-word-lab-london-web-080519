8f0c8b57bd10a83352974f959087a999df41eb07
Aude Faucheux <aude53@gmail.com>
2019-07-27 12:58:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   split_array = sentence.split
   new_reversed = []
-  split_array.each {|word| new_reversed.push(word.reverse)}
+  split_array.collect {|word| new_reversed.push(word.reverse)}
 return new_reversed.join(" ")
 end
